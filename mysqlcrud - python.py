263a9b21b2fbc40fb8a59c513e8ef004033dc81e
lakshmireddy282 <85380215+lakshmireddy282@users.noreply.github.com>
2021-06-07 15:27:37 +0530

    Update mysqlcrud - python.py

--- mysqlcrud - python.py
+++ mysqlcrud - python.py
@@ -5,7 +5,6 @@ import mysql.connector #Importing Connector package
 mysqldb=mysql.connector.connect(host="localhost",user="root",password="")#established connection   
 mycursor=mysqldb.cursor()#cursor() method create a cursor object  
 mycursor.execute("create database dbpython")#Execute SQL Query to create a database    
-mysqldb.close()#Connection Close  
 
 
 mycursor.execute("create table student(roll INT,name VARCHAR(255), marks INT)")#Execute SQL Query to create a table into your database  
